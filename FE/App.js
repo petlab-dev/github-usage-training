44bc536997e5b885035ad755102722709946b2ab
YONGJAE LEE <yongjae.lee@nanocellect.com>
2020-06-27 00:41:19 -0700

    [#1] feat: seperate component of buton

--- FE/App.js
+++ FE/App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-
+import { StyleSheet, Text, View } from 'react-native';
+import ButtonPiece from './ButtonPiece.js';
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-      <Button title="test" />
+      <ButtonPiece buttonTitle="aa" isEnabled={false} />
     </View>
   );
 }
